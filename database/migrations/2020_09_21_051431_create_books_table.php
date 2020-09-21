75ac38ba486f63a3db6fe31c9bb67025ef9023b6
Damir <Damir290598@gmail.com>
2020-09-21 13:44:20 +0600

    done voyager

---
+++ database/migrations/2020_09_21_051431_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('desc');
+            $table->float('price');
+            $table->string('image');
+            $table->unsignedInteger('author_id');
+            $table->unsignedInteger('type_id');
+            $table->unsignedInteger('genre_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
